a94c6789aa4c1283bb4ad68400a45a970bca1aee
nellagio <nickgallo41@gmail.com>
2020-04-13 00:25:45 -0700

    add google map

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var location = {lat: , lng: };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+  }
